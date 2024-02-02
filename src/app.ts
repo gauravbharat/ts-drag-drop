a3fbf068eb07069f206c365b6fbccf60414c6e26
Gaurav Bharat <gaurav.mendse@gmail.com>
2024-02-02 11:51:23 +0530

    render form from template to placeholder

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-// Code goes here!
+class ProjectInput {
+  /** Goal:
+   * Take handle of the project-input template for the form events
+   * Display output in the host element i.e. div#app
+   */
+
+  #templateEl: HTMLTemplateElement;
+  #hostEl: HTMLDivElement;
+  #formEl: HTMLFormElement;
+
+  constructor() {
+    this.#templateEl = <HTMLTemplateElement>(
+      document.getElementById("project-input")
+    );
+    this.#hostEl = <HTMLDivElement>document.getElementById("app");
+
+    const importedNode = document.importNode(this.#templateEl.content, true);
+    this.#formEl = importedNode.firstElementChild as HTMLFormElement;
+    this.#formEl.id = "user-input";
+    this.#attach();
+  }
+
+  #attach(): void {
+    this.#hostEl.insertAdjacentElement("afterbegin", this.#formEl);
+  }
+}
+
+const projectInput = new ProjectInput();
